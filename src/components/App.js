fd543921b90fc05856c1241f778351328570eb3e
Manish kumar <119885517+manishpatel28598@users.noreply.github.com>
2024-06-18 15:09:45 +0530

    add fragment

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 
 const App = () =>{
     return (
-        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        <div>
+            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        </div>
     )
 }
 
